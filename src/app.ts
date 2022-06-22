f943c6b97af8b257124ed80441ec88ddd1af8b7d
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-06-22 20:27:37 +0100

    Set up Typescript
    Following the process of:
    
    1. Run `npx tsc --init`
    2. Moving app files to `/src`
    3. Updating `tsconfig.json` to look in `/src` and output to `/dist`
    4. Updating npm scripts in `package.json` to handle the new directory structure
    5. Renaming .js files to .ts and fixing IDE errors
    6. Adding Typescript compile step to npm scripts
    7. Updating `npm start` to not use `/bin/www` (to simplify the setup)

---
+++ src/app.ts
@@ -0,0 +1,58 @@
+import express from "express";
+import { RequestHandler, ErrorRequestHandler } from "express";
+import createError from "http-errors";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import http from "http";
+import nunjucks from "nunjucks";
+
+import { indexRouter } from "./routes/index";
+
+const app: express.Application = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+nunjucks.configure(
+  ['dist/views', 'node_modules/govuk-frontend/'], 
+  {
+    autoescape:  true,
+    express:  app
+  }
+)
+app.set('view engine', 'njk');
+
+app.use('/assets', express.static(path.join(__dirname, '/node_modules/govuk-frontend/govuk/assets')))
+
+// Set up server 
+
+// catch 404 and forward to error handler
+const notFoundHandler: RequestHandler = (req, res, next) => {
+  next(createError(404));
+};
+app.use(notFoundHandler);
+
+// error handler
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+};
+app.use(errorHandler);
+
+
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+export {router as indexRouter};
