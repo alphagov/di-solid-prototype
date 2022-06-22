04b147c4e40f134ae7d47cbe8d88ee37c18a15c4
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-06-22 18:15:13 +0100

    Set up GOV.UK Frontend
    Following the guides at https://frontend.design-system.service.gov.uk/use-nunjucks
    and https://frontend.design-system.service.gov.uk/importing-css-assets-and-javascript

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 let nunjucks = require('nunjucks')
-
+var path = require('path');
 
 var indexRouter = require('./routes/index');
 
@@ -17,11 +17,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-nunjucks.configure('views', {
-  autoescape:  true,
-  express:  app
-})
-
+nunjucks.configure(
+  ['views', 'node_modules/govuk-frontend/'], 
+  {
+    autoescape:  true,
+    express:  app
+  }
+)
 app.set('view engine', 'njk');
+
+app.use('/assets', express.static(path.join(__dirname, '/node_modules/govuk-frontend/govuk/assets')))
 
 module.exports = app;
