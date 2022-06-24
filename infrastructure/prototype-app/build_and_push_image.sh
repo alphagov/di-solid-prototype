0ff384b0e96c0b4555cb7b626fe2fac813d2568e
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-06-24 14:25:35 +0100

    Set up ECR registry for deployment
    We're going to deploy this app to AWS ECS / Fargate, so we need to
    build a Docker image and host that in a registry. We're already using
    AWS so ECR is the sensible choice. We could use Dockerhub but we'll
    probably hit the free account rate limiting problems at some point and
    procuring a paid account will be more work than setting up ECR.
    
    This commit also adds a bash script which replicates what our CD
    pipeline will eventually do to build, tag and push the app image to
    the new repository.

---
+++ infrastructure/prototype-app/build_and_push_image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euxo pipefail
+
+tag=$(git rev-parse --short HEAD)
+
+ecr_registry=626456592666.dkr.ecr.eu-west-2.amazonaws.com
+ecr_repository=solid-prototype-app
+
+docker build -t $ecr_registry/$ecr_repository:$tag ../../
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin $ecr_registry
+docker push $ecr_registry/$ecr_repository:$tag
