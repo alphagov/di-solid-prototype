8b1b293f5ff6d416d856b1c5639374f9d47e754e
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-07-06 11:12:20 +0100

    GUA-205: Move deployed domain to its own helper

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,12 @@ export function getSessionKeys() {
   return ["key1", "key2"];
 }
 
+export function getDeployedDomain() {
+  return 'prototype.solid.integration.account.gov.uk'
+}
+
 export function getHostname() {
-  const hostname = process.env.NODE_ENV == 'production' ? 'prototype.solid.integration.account.gov.uk' : `localhost:${getPort()}`
+  const hostname = process.env.NODE_ENV == 'production' ? getDeployedDomain() : `localhost:${getPort()}`
   return `${getProtocol()}://${hostname}`;
 }
 
