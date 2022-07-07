0d23df92ac0a5fb4fbbe9d2b75b42fc8cfe0acee
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-07-07 14:29:07 +0100

    GUA-230: Display last accessed time on a page
    When a user first comes to the `/access-logs` page, check if they
    already have a log event in their pod. If they do, display the
    timestamp.
    
    If not, handle the `fetchError` and display an empty state message.
    
    This means we can clean up the POST handler as it now only needs to
    create a dataset if it doesn't exist and write the event to it.
    
    Note: This 'access log' is slightly different to what we'll eventually
    want to implement for the log we display in the account area. This is a
    single `Thing` in the dataset which has a single timestamp property
    which we overwrite each time.
    
    When we build the actual access log we'll need to have a dataset with
    one `Thing` per event that will each have a `DCTERMS.created` timestamp
    so we can maintain the history.

--- src/controllers/access.ts
+++ src/controllers/access.ts
@@ -20,7 +20,18 @@ import { SessionError } from "../errors";
 const GOV_UK_AccessLogEntry = "https://vocab.account.gov.uk/AccessLogEntry";
 
 export async function accessGet(req: Request, res: Response): Promise<void> {
-  res.render('access/show')
+  const session = await getSessionFromStorage(req.session?.sessionId);
+  if (session != undefined) {
+    const datasetUri = await getDatasetUri(session);
+    try {
+      const dataset = await getSolidDataset(datasetUri, {fetch: session.fetch});
+      const created = getDatetime(getThing(dataset, datasetUri) as Thing, DCTERMS.created);
+
+      res.render('access/show', {created: created})      
+    } catch (fetchError) {
+      res.render('access/show')
+    }
+  }
 }
 
 export async function accessPost(req: Request, res: Response): Promise<void> {
@@ -28,13 +39,8 @@ export async function accessPost(req: Request, res: Response): Promise<void> {
   if (session != undefined) {
 
     const datasetUri = await getDatasetUri(session);
-    console.log(`About to try and read resource from Pod at: [${datasetUri}]`);
-
     const dataset = await getOrCreateDataset(session, datasetUri);
 
-    // Rather than create a random 'name' for the Thing in this resource, we
-    // can just use the dataset/resource's full IRI instead (which is not
-    // strictly necessary, but is cleaner).
     const accessLogEntry = buildThing(createThing({ url: datasetUri }))
       .addDatetime(DCTERMS.created, new Date())
       .addUrl(RDF.type, GOV_UK_AccessLogEntry)
@@ -57,19 +63,10 @@ export async function accessPost(req: Request, res: Response): Promise<void> {
 async function getOrCreateDataset(session: Session, datasetUri: string): Promise<SolidDataset> {
   try {
     const dataset = await getSolidDataset(datasetUri, {fetch: session.fetch});
-    console.log(`Successfully read Pod resource at: [${datasetUri}]...`);
-
-    // We know what to look for here (i.e., we know the IRI of the Thing (it's
-    // the same as our Resource/dataset IRI), and we know to look for a
-    // DCTERMS.created property).
-    const created = getDatetime(getThing(dataset, datasetUri) as Thing, DCTERMS.created);
-    console.log(`Previous DCTERMS.created value: [${created}]`);
-
-    return dataset
+    return dataset;
   } catch (fetchError) {
-    console.log(`Failed to fetch (a 404 is fine, as this could be our first time with this Pod!) resource at [${datasetUri}] - error: [${fetchError}]`);
     const dataset = createSolidDataset();
-    return dataset
+    return dataset;
   }
 }
 
@@ -81,9 +78,3 @@ async function getDatasetUri(session: Session) {
     throw new SessionError();
   }
 }
-
-// Not needed at the moment, but causes TS warning if not commented out, so
-// leaving here in case you wish to re-instate it later.
-// function generateId() {
-//   return Math.random().toString(36).replace(/[^a-z]+/g, '').substring(0, 5);
-// }
