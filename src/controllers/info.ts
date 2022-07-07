7aac599038cf675c93af826734711916ad4f44e1
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-07-07 11:28:25 +0100

    GUA-230: Allow fetching a refresh token
    Update the Solid-OIDC client ID document to include the scopes and
    grant types that allow the app to request a refresh token.
    
    This is because we need a refresh token in order to make further calls
    to ESS on pages after the login callback. The Solid client library will
    handle including the refresh token in any further calls of `Session.fetch`
    so this should be all that's needed to get the read / write functionality
    in #37 working again.

--- src/controllers/info.ts
+++ src/controllers/info.ts
@@ -19,7 +19,9 @@ export function buildClientIdDocument(): ClientIdDocument {
     "redirect_uris": [
       `${getHostname()}/login/callback`,
       "http://localhost:3000/login/callback"
-    ]
+    ],
+    "scope" : "openid profile offline_access webid",
+    "grant_types" : ["refresh_token","authorization_code"],
   }
 }
 
@@ -30,4 +32,6 @@ export type ClientIdDocument = {
   client_uri: string;
   post_logout_redirect_uris: string[];
   redirect_uris: string[];
+  scope: string;
+  grant_types: string[];
 }
