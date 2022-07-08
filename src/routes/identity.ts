dc54ccb195130d0ce3045c962c397c1caa7bb09f
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2022-07-08 09:49:40 +0100

    Offer clickable login route for /identity/save
    We'll need to work out how to pass a redirect URI shortly
    
    Intered to dig more into the relationship between these routes and the
    ID document
    
    I'm guessing this'll work pretty similar to how we had frontend set up
    for the account-api work. So there was a callback URL there that
    everything went to as the front door... and then that bit read off a
    parameter and send the user back to where they'd started :thinking:

--- src/routes/identity.ts
+++ src/routes/identity.ts
@@ -1,9 +1,30 @@
 import express from "express";
-import { saveGet, savePost } from "../controllers/identity/save"
+import { Request, Response, NextFunction } from "express";
+import {
+    saveGet,
+    savePost,
+} from "../controllers/identity/save"
+
+import { getSessionFromStorage } from "@inrupt/solid-client-authn-node";
 
 const router = express.Router();
 
-/* GET Save your identity page. */
+async function redirectIfNotLoggedIn(req: Request, res: Response, next: NextFunction) {
+    if (req.session == undefined) { res.redirect("/login") }
+    const session = await getSessionFromStorage(req.session?.sessionId);
+  
+    if (!session?.info.isLoggedIn) {
+      res.redirect("/login");
+    } else {
+      next()
+    }
+  }
+
+router.use((req: Request, res: Response, next: NextFunction) => {
+    redirectIfNotLoggedIn(req, res, next);
+})
+
+/* Save your identity page. */
 router.get('/save', saveGet);
 router.post('/save', savePost);
 
