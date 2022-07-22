bcdae801cef0e03b81b1cf967822b228e3cd0537
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-07-22 16:23:42 +0100

    GUA-209: Store KBV check in pod
    This commit also includes some new helper methods to get common data
    eg. address, name etc. from the session and into the correct format.
    
    This does mean that the name information in both the passport and KBV
    credentials comes from the same source whereas in the real system
    they're generated separately by each CRI. What we have here is fine for
    our PoC though.

--- src/controllers/identity/save.ts
+++ src/controllers/identity/save.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express";
 import { passportCheckVC } from "../../lib/passport_check_vc";
-import { evidenceSuccessful, generateJWT } from "../../lib/credential_helpers";
+import { kbvCheckVC } from "../../lib/kvb_check_vc"
+import { 
+  evidenceSuccessful,
+  generateJWT,
+  getBirthDate,
+  getNameParts,
+  getPostalAddress
+} from "../../lib/credential_helpers";
 import { getSessionFromStorage } from "@inrupt/solid-client-authn-node";
-import {
-  NamePart,
-} from "../../components/vocabularies/CommonComponents";
-
 
 import {
   buildThing,
@@ -41,6 +44,9 @@ import { RDF } from "@inrupt/vocab-common-rdf";
 // eslint-disable-next-line no-shadow
 import { Blob } from "node:buffer";
 
+const GOV_UK_CREDENTIAL = "https://vocab.account.gov.uk/GovUKCredential";
+const GOV_UK_hasCredential = "https://vocab.account.gov.uk/hasCredential";
+
 export function saveGet(req: Request, res: Response) {
   res.render('identity/save');
 }
@@ -49,17 +55,14 @@ export async function savePost(req: Request, res: Response): Promise<void> {
   const session = await getSessionFromStorage(req.session?.sessionId);
 
   if (session != undefined && req.session) {
+    req.session.webId = session.info.webId
     const containerUri = await getDatasetUri(session, "private/govuk/identity/poc/credentials-pat/vcs");
     
     const passportArtifacts = await buildPassportCheckArtifacts(req.session, containerUri);
     await writeCheckToPod(session, passportArtifacts)
 
-    // @TODO All the above will amount to saving the passport check.
-    // It needs some of the linke data GOV_UK_* updated to reflect that
-    // Also we then need to repeat this below, but generating a second JWT that represents
-    // The KVB check... That will need a _new_ function here similar to buildPassportIdentityCheck()
-    // but can import from kvb_check_vc.ts, then we'll need more linked data.
-    // After that's complete we can then redirect to saved!
+    const kbvArtifacts = await buildKbvCheckArtifacts(req.session, containerUri)
+    await writeCheckToPod(session, kbvArtifacts)
 
     res.redirect('/identity/complete/saved');
   } else {
@@ -67,30 +70,7 @@ export async function savePost(req: Request, res: Response): Promise<void> {
   }
 }
 
-async function buildPassportCheck(session: CookieSessionInterfaces.CookieSessionObject): Promise<string> {
-  const firstName: string = session.passport["first-name"]
-  const middleName: string = session.passport["middle-name"]
-  const surname: string = session.passport["surname"]
-
-  const nameParts: NamePart[] = [
-    {
-      "value": firstName,
-      "type": "GivenName",
-    },
-    {
-      "value": middleName,
-      "type": "GivenName",
-    },
-    {
-      "value": surname,
-      "type": "FamilyName",
-    },
-  ]
-
-  const byear = session.passport["date-of-birth-year"]
-  const bmonth = session.passport["date-of-birth-year"]
-  const bday = session.passport["date-of-birth-day"]
-  const birthDate = `${byear}-${bmonth}-${bday}`
+function buildPassportCheck(session: CookieSessionInterfaces.CookieSessionObject): string {
   
   const eyear = session.passport["date-of-birth-year"]
   const emonth = session.passport["date-of-birth-year"]
@@ -101,27 +81,23 @@ async function buildPassportCheck(session: CookieSessionInterfaces.CookieSession
   }
 
   const payload = passportCheckVC(
-    nameParts,
-    birthDate,
+    getNameParts(session),
+    getBirthDate(session),
     passportDetails,
     evidenceSuccessful()
   )
 
-  const solidSession = await getSessionFromStorage(session.sessionId);
-  return generateJWT(payload, solidSession?.info.webId || "")
+  return generateJWT(payload, session.webId)
 }
 
 async function buildPassportCheckArtifacts(
   session: CookieSessionInterfaces.CookieSessionObject,
   containerUri: string
 ): Promise<CheckArtifacts> {
-  const GOV_UK_CREDENTIAL = "https://vocab.account.gov.uk/GovUKCredential";
-  const GOV_UK_hasCredential = "https://vocab.account.gov.uk/hasCredential";
-
   const fileUri = `${containerUri}/passport/check`;
   const metadataUri = `${containerUri}/passport/metadata`;
 
-  const file = new Blob([await buildPassportCheck(session)], { type: "application/json" })
+  const file = new Blob([buildPassportCheck(session)], { type: "application/json" })
 
   const metadata = buildThing(
     createThing({ url: metadataUri })
@@ -142,4 +118,39 @@ export interface CheckArtifacts {
   fileUri: string,
   metadata: Thing,
   metadataUri: string
+}
+
+function buildKbvCheck(session: CookieSessionInterfaces.CookieSessionObject): string {
+  const payload = kbvCheckVC(
+    getNameParts(session),
+    getBirthDate(session),
+    getPostalAddress(session),
+    evidenceSuccessful()
+  )
+
+  return generateJWT(payload, session.webId)
+}
+
+async function buildKbvCheckArtifacts(
+  session: CookieSessionInterfaces.CookieSessionObject,
+  containerUri: string
+): Promise<CheckArtifacts> {
+  const fileUri = `${containerUri}/kbv/check`;
+  const metadataUri = `${containerUri}/kbv/metadata`;
+
+  const file = new Blob([buildKbvCheck(session)], { type: "application/json" })
+
+  const metadata = buildThing(
+    createThing({ url: metadataUri })
+  )
+  .addUrl(RDF.type, GOV_UK_CREDENTIAL)
+  .addUrl(GOV_UK_hasCredential, fileUri)
+  .build();
+
+  return {
+    file: file,
+    fileUri: fileUri,
+    metadata: metadata,
+    metadataUri: metadataUri
+  }
 }
