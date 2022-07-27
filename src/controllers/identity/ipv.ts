2c4106fa7edee05da7b224bf835fb8384e578af1
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-07-27 11:46:46 +0100

    GUA-231: Check if a user has stored checks at the start of the DBS journey
    This means if someone completes the IPV journey successfully (through
    any source) and then starts the DBS journey they'll skip the questions.

--- src/controllers/identity/ipv.ts
+++ src/controllers/identity/ipv.ts
@@ -20,7 +20,7 @@ export function proveIdentityLoggedOutGet(req: Request, res: Response) {
 
 export function proveIdentityLoggedOutPost(req: Request, res: Response) {
   if (req.body["prove-identity-options"] == "account") {
-    res.redirect("/identity/prove-identity");
+    res.redirect("/identity");
   } else if (req.body["prove-identity-options"] == "in-person") {
     res.redirect("/identity/check-in-person");
   }
