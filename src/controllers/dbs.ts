cc66974994083945687da3fe17ae6aa0e349574a
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-08-01 10:39:30 +0100

    GUA-231: Make DBS controllers synchronous
    None of these controllers are interacting with the pod or making any
    other external requests, so they don't need to be `async`.

--- src/controllers/dbs.ts
+++ src/controllers/dbs.ts
@@ -1,10 +1,7 @@
 import { Request, Response } from "express";
 import { getHostname } from "../config";
 
-export async function dbsContentPageGet(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function dbsContentPageGet(req: Request, res: Response): void {
   if (req.session) {
     req.session.journey = {
       nextPage: `${getHostname()}/dbs/apply-for-a-basic-dbs-check`,
@@ -14,10 +11,7 @@ export async function dbsContentPageGet(
   res.render("dbs/request-a-basic-dbs-check");
 }
 
-export async function proveYourIdentityGet(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function proveYourIdentityGet(req: Request, res: Response): void {
   res.render("dbs/prove-your-identity");
 }
 
@@ -25,87 +19,54 @@ export function proveYourIdentityPost(req: Request, res: Response): void {
   res.redirect(`${getHostname()}/identity/prove-identity-logged-out`);
 }
 
-export async function applyGet(req: Request, res: Response): Promise<void> {
+export function applyGet(req: Request, res: Response): void {
   res.render("dbs/apply-for-a-basic-dbs-check");
 }
 
-export async function otherNamesGet(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function otherNamesGet(req: Request, res: Response): void {
   res.render("dbs/other-names");
 }
 
-export async function whatIsYourSexGet(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function whatIsYourSexGet(req: Request, res: Response): void {
   res.render("dbs/what-is-your-sex");
 }
 
-export async function whereWereYouBornGet(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function whereWereYouBornGet(req: Request, res: Response): void {
   res.render("dbs/where-were-you-born");
 }
 
-export async function ninoGet(req: Request, res: Response): Promise<void> {
+export function ninoGet(req: Request, res: Response): void {
   res.render("dbs/nino");
 }
 
-export async function drivingLicenceGet(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function drivingLicenceGet(req: Request, res: Response): void {
   res.render("dbs/driving-licence");
 }
 
-export async function certificateAddressWhereGet(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function certificateAddressWhereGet(req: Request, res: Response): void {
   res.render("dbs/certificate-address-where");
 }
 
-export async function emailAddressGet(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function emailAddressGet(req: Request, res: Response): void {
   res.render("dbs/email-address");
 }
 
-export async function mobileNumberGet(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function mobileNumberGet(req: Request, res: Response): void {
   res.render("dbs/mobile-number");
 }
 
-export async function whoIsPayingGet(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function whoIsPayingGet(req: Request, res: Response): void {
   res.render("dbs/who-paying");
 }
 
-export async function checkYourDetailsGet(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function checkYourDetailsGet(req: Request, res: Response): void {
   res.render("dbs/check-your-details");
 }
 
-export async function reviewYourApplicationGet(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function reviewYourApplicationGet(req: Request, res: Response): void {
   res.render("dbs/review-your-application");
 }
 
-export async function disclaimerGet(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function disclaimerGet(req: Request, res: Response): void {
   res.render("dbs/disclaimer");
 }
