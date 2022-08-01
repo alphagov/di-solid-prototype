3fc139e44af73ba556afeb863426a3cb57bbbb53
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2022-08-01 12:28:46 +0100

    GUA-231: Wire up Personal Tax Account

--- src/routes/personalTax.ts
+++ src/routes/personalTax.ts
@@ -1,14 +1,19 @@
 import express from "express";
 import {
   signInOrSetUpGet,
+  signInOrSetUpPost,
   homeGet,
   choosePaperlessGet,
+  choosePaperlessPost,
 } from "../controllers/personalTax";
 
 const router = express.Router();
 
 router.get("/home", homeGet);
 router.get("/sign-in-or-set-up", signInOrSetUpGet);
+router.post("/sign-in-or-set-up", signInOrSetUpPost);
+
 router.get("/choose-paperless", choosePaperlessGet);
+router.post("/choose-paperless", choosePaperlessPost);
 
 export default router;
