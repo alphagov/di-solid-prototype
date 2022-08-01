157626e1ca5640a279ec0d2e7fece485b747ebb7
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2022-08-01 17:06:27 +0100

    GUA-231: Place PTA routes behind login
    Move Home to the bottom too

--- src/routes/personalTax.ts
+++ src/routes/personalTax.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import {
   signInOrSetUpGet,
   signInOrSetUpPost,
@@ -7,13 +7,21 @@ import {
   choosePaperlessPost,
 } from "../controllers/personalTax";
 
+import redirectIfNotLoggedIn from "../lib/middleware/redirectIfNotLoggedIn";
+
 const router = express.Router();
 
-router.get("/home", homeGet);
 router.get("/sign-in-or-set-up", signInOrSetUpGet);
 router.post("/sign-in-or-set-up", signInOrSetUpPost);
 
+/* All following routes require someone to be logged in first */
+router.use((req: Request, res: Response, next: NextFunction) => {
+  redirectIfNotLoggedIn(req, res, next);
+});
+
 router.get("/choose-paperless", choosePaperlessGet);
 router.post("/choose-paperless", choosePaperlessPost);
+
+router.get("/home", homeGet);
 
 export default router;
