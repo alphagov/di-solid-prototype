83daa96b1bd1b2b27a336b1a18260651efa48109
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2022-08-01 17:07:16 +0100

    GUA-231: Send the user to the identity check with session params
    Title to render the button that returns them,
    nextPage to send them back to the right place.
    
    Shoulders of Giants this is all re-using code Alex wrote for the DBS
    journey (nice stuff :D!)

--- src/controllers/personalTax.ts
+++ src/controllers/personalTax.ts
@@ -1,21 +1,28 @@
 import { Request, Response } from "express";
+import { getHostname } from "../config";
+
+export async function homeGet(req: Request, res: Response): Promise<void> {
+  res.render("personal-tax/home");
+}
 
 export async function signInOrSetUpGet(
   req: Request,
   res: Response
 ): Promise<void> {
+  if (req.session) {
+    req.session.journey = {
+      nextPage: `${getHostname()}/personal-tax/choose-paperless`,
+      title: "apply for personal tax account",
+    };
+  }
   res.render("personal-tax/sign-in-or-set-up");
-}
-
-export async function homeGet(req: Request, res: Response): Promise<void> {
-  res.render("personal-tax/home");
 }
 
 export async function signInOrSetUpPost(
   req: Request,
   res: Response
 ): Promise<void> {
-  res.redirect("/personal-tax/choose-paperless");
+  res.redirect("/identity");
 }
 
 export async function choosePaperlessGet(
