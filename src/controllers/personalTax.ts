af639cc173b35d63a74390fefc3caed78e3bc917
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2022-08-02 08:47:53 +0100

    BAU: Remove redundant async from personalTax

--- src/controllers/personalTax.ts
+++ src/controllers/personalTax.ts
@@ -1,14 +1,11 @@
 import { Request, Response } from "express";
 import { getHostname } from "../config";
 
-export async function homeGet(req: Request, res: Response): Promise<void> {
+export function homeGet(req: Request, res: Response): void {
   res.render("personal-tax/home");
 }
 
-export async function signInOrSetUpGet(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function signInOrSetUpGet(req: Request, res: Response): void {
   if (req.session) {
     req.session.journey = {
       nextPage: `${getHostname()}/personal-tax/choose-paperless`,
@@ -18,23 +15,14 @@ export async function signInOrSetUpGet(
   res.render("personal-tax/sign-in-or-set-up");
 }
 
-export async function signInOrSetUpPost(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function signInOrSetUpPost(req: Request, res: Response): void {
   res.redirect("/identity");
 }
 
-export async function choosePaperlessGet(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function choosePaperlessGet(req: Request, res: Response): void {
   res.render("personal-tax/choose-paperless");
 }
 
-export async function choosePaperlessPost(
-  req: Request,
-  res: Response
-): Promise<void> {
+export function choosePaperlessPost(req: Request, res: Response): void {
   res.redirect("/personal-tax/home");
 }
