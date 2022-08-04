405007b9dd6cba208a327fc60627cc643a3efa26
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-08-04 11:14:48 +0100

    BAU: Add prototype end page
    We'll add this to the end of each service journey so users know when
    they've finished and have a route back to start another journey.

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
 
-function indexGet(req: Request, res: Response): void {
+export function indexGet(req: Request, res: Response): void {
   res.render("index");
+}
+
+export function journeyEndGet(req: Request, res: Response): void {
+  res.render("end");
 }
 
 export default indexGet;
