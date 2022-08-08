e4e9480b3fee49d2f6bb99f9cfc0c56ed4a576d2
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2022-08-08 16:13:34 +0100

    GUA-314: Set up the rest of the NI checker
    Including a `/` and `/continue` route which will be used later to
    accept a return URI at the beginning and redirect the user back
    correctly after verifying their NI number.

--- src/controllers/nino.ts
+++ src/controllers/nino.ts
@@ -1,9 +1,29 @@
 import { Request, Response } from "express";
 
+export function startGet(req: Request, res: Response): void {
+  res.redirect("/nino/enter-your-number");
+}
+
 export function enterNinoGet(req: Request, res: Response): void {
   res.render("nino/enter-your-number");
 }
 
 export function enterNinoPost(req: Request, res: Response): void {
-  res.redirect("/nino/enter-your-number");
+  res.redirect("/nino/weve-verified-your-number");
+}
+
+export function verifiedNinoGet(req: Request, res: Response): void {
+  res.render("nino/weve-verified-your-number");
+}
+
+export function verifiedNinoPost(req: Request, res: Response): void {
+  res.redirect("/nino/youve-saved-your-number");
+}
+
+export function savedNinoGet(req: Request, res: Response): void {
+  res.render("nino/youve-saved-your-number");
+}
+
+export function continueGet(req: Request, res: Response): void {
+  res.redirect("/");
 }
--- src/routes/nino.ts
+++ src/routes/nino.ts
@@ -1,9 +1,22 @@
 import express from "express";
-import { enterNinoGet, enterNinoPost } from "../controllers/nino";
+import {
+  continueGet,
+  enterNinoGet,
+  enterNinoPost,
+  startGet,
+  verifiedNinoGet,
+  verifiedNinoPost,
+  savedNinoGet,
+} from "../controllers/nino";
 
 const router = express.Router();
 
+router.get("/", startGet);
 router.get("/enter-your-number", enterNinoGet);
 router.post("/enter-your-number", enterNinoPost);
+router.get("/weve-verified-your-number", verifiedNinoGet);
+router.post("/weve-verified-your-number", verifiedNinoPost);
+router.get("/youve-saved-your-number", savedNinoGet);
+router.get("/continue", continueGet);
 
 export default router;
